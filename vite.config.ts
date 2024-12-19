542ead558d511be4ec92e8f449aae723efe0746b
Wing <me@nikowong.com>
2024-12-19 00:13:17 +0000

    gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), ghPages()],
+	base: "/flight-ics-generator/",
+});
