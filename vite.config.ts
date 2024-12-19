06f081f1157de26d0987b47b81a929e332a4c9ff
Wing <me@nikowong.com>
2024-12-19 00:22:14 +0000

    base typo

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { ghPages } from "vite-plugin-gh-pages";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), ghPages()],
-	base: "/flight-ics-generator/",
+	base: "/flight-calendar-app/",
 });
