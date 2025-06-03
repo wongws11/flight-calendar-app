0e11c75ecfc21ca9c84d6795f08a8370d0d6f168
Wing <me@nikowong.com>
2025-06-03 23:11:02 +0100

    autocomplete for airports performance big boost

--- src/components/AirportAutocomplete.tsx
+++ src/components/AirportAutocomplete.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Autocomplete, TextField } from "@mui/material";
 import { Airport, airports } from "../data/airports";
 
@@ -11,14 +11,37 @@ const AirportAutocomplete: React.FC<AirportAutocompleteProps> = ({
 	label,
 	onChange,
 }) => {
+	const [options, setOptions] = useState<Airport[]>([]);
+
+	const filterAirports = (input: string) => {
+		if (input.length < 3) {
+			setOptions([]);
+			return;
+		}
+
+		const filteredAirports = airports.reduce<Airport[]>((acc, cur) => {
+			if (cur.iata === input.toUpperCase()) {
+				return [...acc, cur];
+			}
+			if (cur.name.toLowerCase().includes(input.toLowerCase())) {
+				return [...acc, cur];
+			}
+			return acc;
+		}, []);
+		setOptions(filteredAirports);
+	};
+
 	return (
 		<Autocomplete
-			options={airports}
+			options={options}
 			getOptionLabel={(option) => `${option.iata} - ${option.name}`}
-			onChange={(_event, value) => onChange(value || null)}
+			onChange={(_e, value) => onChange(value || null)}
 			renderInput={(params) => (
 				<TextField {...params} label={label} variant="outlined" />
 			)}
+			onInputChange={(_e, value) => {
+				filterAirports(value);
+			}}
 		/>
 	);
 };
